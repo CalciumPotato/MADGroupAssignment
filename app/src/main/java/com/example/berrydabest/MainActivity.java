6b8981e608e0cdbcc26871c81155b03fccf93bfa
KingsleysSann <141652804+KingsleysSann@users.noreply.github.com>
2023-08-26 18:18:02 +0800

    Login Interface

--- app/src/main/java/com/example/berrydabest/MainActivity.java
+++ app/src/main/java/com/example/berrydabest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.berrydabest;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView signup = findViewById(R.id.sign);
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+
+        signup.setPaintFlags(signup.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
